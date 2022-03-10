0b35b8951004f1f0ac9d07243df47f1e8cb1037e
Ajay Kumar <Ajaykumar18447@gmail.com>
2022-03-10 23:10:10 +0530

    MAximum SubArray

---
+++ kadane.java
@@ -0,0 +1,25 @@
+package interviewCodingQuestion;
+
+public class kadane {
+    public static  int maxSub(int[] nums){
+        int sum = 0;
+        int max = nums[0];
+
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum>max){
+                max = sum;
+            }if(sum<0){
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+    public static void main(String[] args) {
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+        System.out.println(maxSub(arr));
+    }
+    
+}
