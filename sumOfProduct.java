4e05ddc2e04b5180cf4225662e9d8ccf9648bd81
Ajay Kumar <Ajaykumar18447@gmail.com>
2022-03-17 00:08:46 +0530

    Sum of Product

--- sumOfProduct.java
+++ sumOfProduct.java
@@ -11,6 +11,8 @@ explanation - 22*(-3)+7*10+1*12+-5*21+5*-1+-2*4 = -102
 */
 package interviewCodingQuestion;
 
+import java.util.Scanner;
+
 public class sumOfProduct {
     public static int product(int arr1[],int arr2[] , int n){
         int sum=0;
@@ -24,10 +26,19 @@ public class sumOfProduct {
         
     }
     public static void main(String[] args) {
-        int arr1[] = {22,7,1,-5,5,-2};
-        int arr2[] = {4,-1,21,12,10,-3};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr1[] = new int[n];
+        int arr2[] = new int[n];
 
-        int n = arr1.length;
+        for(int i=0;i<n;i++){
+            arr1[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            arr2[i] = sc.nextInt();
+        }
+        
+       
         System.out.println(product(arr1, arr2, n));
 
     }
